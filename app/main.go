3be85b07d2b38ba2dddc670fcee2bb936b3e5da9
yusufnuru <yusufnuru2002@gmail.com>
2026-05-29 14:57:05 +0800

    feat: implement a REPL [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,14 +7,18 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Print("$ ")
+
+		// Wait for user input
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
